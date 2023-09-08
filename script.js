1d5c9b1d2611bc3f7f6b06370ad3f81957ba9202
Vidit <viditgoel2k@gmail.com>
2023-09-08 23:34:59 +0530

    game implemented

--- script.js
+++ script.js
@@ -1,2 +1,35 @@
-let randomNumber = (parseInt((Math.random()*100)+1))
+const randomNumber = (parseInt((Math.random()*100)+1))
+const form = document.querySelector("#form")
 
+const wrapper = document.querySelector("#wrapper")
+
+let prevGuessArr = []
+
+form.addEventListener("submit", (e) => {
+    e.preventDefault();
+
+    const guess = parseInt(document.querySelector("#guessField").value)
+
+    if(guess === randomNumber){
+        wrapper.innerText = `You Won!`
+    }
+    else{
+        const prevGuesses = document.querySelector(".prevGuesses")
+        prevGuessArr.push(guess)
+        prevGuesses.innerText = `${prevGuessArr}`
+
+        const guessRemainingDiv = document.querySelector(".guessRemaining")
+
+        const guessRemaining = parseInt(guessRemainingDiv.innerText)
+
+        if(guessRemaining === 1){
+        wrapper.innerText = `You Lost!`
+        }
+
+        guessRemainingDiv.innerText = `${guessRemaining-1}`
+
+    }
+
+
+
+})
